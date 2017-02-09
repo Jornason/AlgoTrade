313d9451cef70a13ac38e6c6acdefa47384f6931
John Anderson <jander43@vols.utk.edu>
2017-02-09 11:23:32 -0500

    Updated with attribution

--- tutorial3.py
+++ tutorial3.py
@@ -4,7 +4,10 @@
 Author: John D. Anderson
 Email: jander43@vols.utk.edu
 Description:
-    Simple trading strategy for 'PyAlgoTrade' tutorial
+    Simple trading strategy from 'PyAlgoTrade' tutorial (see Attribution)
+Attribution:
+    Gabriel Becedillas 2011-2015
+    http://gbeced.github.io/pyalgotrade/docs/v0.18/html/tutorial.html
 Usage:
     tutorial3
 '''
